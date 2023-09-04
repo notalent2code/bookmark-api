63f6d09b3460a45033cba9af2d34e3e183c08f54
notalent2code <rihlanrihlan24@gmail.com>
2023-09-04 14:40:56 +0700

    feat: create auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    const isPasswordValid = await argon.verify(user.hash, dto.password);
+
+    if (!isPasswordValid) {
+      throw new ForbiddenException('Invalid credentials');
+    }
+
+    delete user.hash;
+
+    return user;
+  }
+}
