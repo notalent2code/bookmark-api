5f2ee19204bcfc502f894ab80e38fdc918fd1ca1
notalent2code <rihlanrihlan24@gmail.com>
2023-09-05 08:09:50 +0700

    feat: implement main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { BookmarkModule } from 'src/bookmark/bookmark.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   imports: [
